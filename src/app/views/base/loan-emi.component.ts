7ade9bc0fbbf119178734de073c8d66965ce79fa
digambar <digambar@pixel6.co>
2022-06-04 11:16:44 +0530

    date check correction

--- src/app/views/base/loan-emi.component.ts
+++ src/app/views/base/loan-emi.component.ts
@@ -4,6 +4,7 @@ import { LoanserviceService } from '../../Services/loanservice.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Loandetail } from '../../Module/loandetail';
 import { PaymentDetail } from '../../Module/payment-detail';
+import * as moment from 'moment';
 
 @Component({
     templateUrl: 'loan-emi.component.html'
@@ -11,7 +12,7 @@ import { PaymentDetail } from '../../Module/payment-detail';
 export class LoanEMIComponent {
     allCustomerList: any;
     loanPaymetDetails: Array<PaymentDetail> = [];
-    loanDetail: Loandetail = new Loandetail(null);
+    loanDetail: any;
     loanPaymentDetail: PaymentDetail = new PaymentDetail(null);
     isCollapsed: boolean = false;
     isValidate: boolean = false;
@@ -19,7 +20,10 @@ export class LoanEMIComponent {
     currDate = new Date();
     collectionType = "Daily";
     iconCollapse: string = 'icon-arrow-up';
-    constructor(private customerService: CustomerserviceService, private loanservice: LoanserviceService, private router: Router, private route: ActivatedRoute) { }
+    constructor(private customerService: CustomerserviceService, private loanservice: LoanserviceService, private router: Router, private route: ActivatedRoute) {
+        this.loanDetail = { loanAccountNo: 4, "custId": 4, "principalAmount": 100000.0, "interest": 10.0, "interestAmt": 10000.0, "depositeAmt": 5000.0, "processingFees": 500.0, "loanAmt": 84500.0, "loanStartDate": "2022-06-10", "loanEndDate": "2022-06-22", "installMentType": "Daily", "installments": 100, "installmentAmount": 1000.0, "custFullName": "Rahul Patil", "totalCollection": 70000.0, "loanStatus": "Closed", "remark": "", "disburseAmt": 84500.0, "paymentDate": "2022-06-02", "paymentMode": "", "loanCollections": [{ "loanAccNo": null, "payment": 10000.0, "paymentMethod": "Daily", "paymentDate": "2022-06-03", "paymentId": 9 }, { "loanAccNo": null, "payment": 10000.0, "paymentMethod": "Daily", "paymentDate": "2022-06-03", "paymentId": 10 }, { "loanAccNo": null, "payment": 10000.0, "paymentMethod": "Daily", "paymentDate": "2022-06-03", "paymentId": 11 }, { "loanAccNo": null, "payment": 10000.0, "paymentMethod": "Daily", "paymentDate": "2022-06-03", "paymentId": 12 }, { "loanAccNo": null, "payment": 10000.0, "paymentMethod": "Daily", "paymentDate": "2022-06-04", "paymentId": 13 }, { "loanAccNo": null, "payment": 10000.0, "paymentMethod": "Daily", "paymentDate": "2022-06-04", "paymentId": 14 }, { "loanAccNo": null, "payment": 10000.0, "paymentMethod": "Daily", "paymentDate": "2022-06-04", "paymentId": 15 }] }
+        this.loanPaymetDetails = this.loanDetail.loanCollections;
+    }
     collapsed(event: any): void {
         // console.log(event);
     }
@@ -58,50 +62,40 @@ export class LoanEMIComponent {
     }
 
     addCustomerPayment(paymentDetail: any) {
-        this.isValidate = true;
         this.loanPaymentDetail = paymentDetail;
         this.loanPaymentDetail.loanAccNo = this.loanDetail.loanAccountNo;
         this.loanPaymentDetail.paymentMethod = "Daily";
-        this.loanservice.checkDuplicatePayment(this.loanDetail.loanAccountNo, paymentDetail.paymentDate).subscribe(data => {
-            if (data) {
-                this.checkDuplicate = true;
-            }
-        })
-        if (this.checkDuplicate) {
-            if (confirm("Payment Already Added On This Date,Are You Want Add Again")) {
-                this.checkDuplicate = true;
-            }
-        }
+
         if (this.loanDetail.loanAccountNo == "" || this.loanDetail.loanAccountNo == undefined) {
             alert("Please enter loan id")
-            this.isValidate = false;
+            return;
         }
         if (paymentDetail.payment == "" || paymentDetail.payment == undefined) {
             alert("Please enter payment")
-            this.isValidate = false;
+            return;
         }
         if (paymentDetail.paymentDate == "" || paymentDetail.paymentDate == undefined) {
             alert("Please enter payment date")
-            this.isValidate = false;
-        }
-        let paymentDate = this.loanPaymetDetails.find(e => e.paymentDate == paymentDetail.paymentDate);
-
-        if (paymentDate) {
-            alert("EMI is already paid");
             return;
         }
 
-        if (this.isValidate && !this.checkDuplicate) {
-            this.loanservice.addCustomerPaymet(this.loanPaymentDetail).subscribe(data => {
-                if (this.loanPaymetDetails == null) {
-                    this.loanPaymetDetails = [];
-                }
-                this.loanPaymetDetails.push(this.loanPaymentDetail);
-                alert("Added Successfully");
-                this.checkDuplicate = false;
-            })
+        let paymentDate = this.loanPaymetDetails.find(e => e.paymentDate == paymentDetail.paymentDate);
+        console.log(paymentDetail);
+        console.log(paymentDate);
+        
+        if (paymentDate) {
+            alert("Payment Already Added On This Date, Are You Want Add Again");
+            return;
         }
 
+        this.loanservice.addCustomerPaymet(this.loanPaymentDetail).subscribe(data => {
+            if (this.loanPaymetDetails == null) {
+                this.loanPaymetDetails = [];
+            }
+            this.loanPaymetDetails.push(this.loanPaymentDetail);
+            alert("Added Successfully");
+            this.checkDuplicate = false;
+        })
     };
 
 
